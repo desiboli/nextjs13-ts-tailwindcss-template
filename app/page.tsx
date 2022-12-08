29acb89f954dc33fa09e7499eba661de2e5f3757
Mustafa Adam Alshammaa <adam.benals@gmail.com>
2022-12-08 15:52:15 +0100

    layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <main className="min-h-screen flex flex-1 items-center justify-center">
       <h1 className="text-3xl font-bold">
         Nextjs Typescript +{' '}
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
@@ -8,6 +8,6 @@ export default function Home() {
         </span>{' '}
         Template
       </h1>
-    </div>
+    </main>
   );
 }
